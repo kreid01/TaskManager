48a6ad046fa8903df85d024212187ba231cf262d
kreid01 <kieranreid2014@icloud.com>
2022-11-18 23:30:39 +0000

    Implemented access tokens with logging in.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,125 @@
 "use client";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-} from "@apollo/react-hooks";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { ApolloClient } from "apollo-client";
+import { getAccessToken, setAccessToken } from "./accessToken";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      let handle: any;
+      Promise.resolve(operation)
+        .then((operation) => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `bearer ${accessToken}`,
+              },
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001/graphql",
-  cache: new InMemoryCache(),
-});
+  link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: "accessToken",
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
 
+        if (!token) {
+          return true;
+        }
+
+        try {
+          const { exp } = jwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch("http://localhost:3001/refresh_token", {
+          method: "POST",
+          credentials: "include",
+        });
+      },
+      handleFetch: (accessToken: string) => {
+        setAccessToken(accessToken);
+      },
+      handleError: (err: string) => {
+        console.warn("Your refresh token is invalid. Try to relogin");
+        console.error(err);
+      },
+    }),
+    onError(({ graphQLErrors, networkError }) => {
+      console.log(graphQLErrors);
+      console.log(networkError);
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:3001/graphql",
+      credentials: "include",
+    }),
+  ]),
+  cache,
+});
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://localhost:3001/refresh_token", {
+      method: "POST",
+      credentials: "include",
+    }).then(async (x) => {
+      const { accessToken } = await x.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <html>
       <body>
         <main>
           <nav>
             <Link href="register">Register</Link>
-            <Link href="login">Link</Link>
+            <Link href="login">Login</Link>
             <Link href="/">Home</Link>
           </nav>
           <ApolloProvider client={client}>{children}</ApolloProvider>
